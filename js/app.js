a0cf1a475d8a40c9b82ce159ec892b394099d10f
TikenDev <webdesignerfran@gmail.comgit version 2.30.1 (Apple Git-130)tiken@MacBook-Pro-de-Tiken ~ % git config --global user.name TikenDevtiken@MacBook-Pro-de-Tiken ~ % git config --global user.email webdesignerfran@gmail.com>
2022-07-09 11:06:57 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,150 @@
+// Utilizar expresiones regulares para validar formularios de manera correcta:
+const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+// Variables:
+const btnEnviar = document.querySelector('#enviar');
+const btnReset = document.querySelector('#resetBtn')
+const formulario = document.querySelector('#enviar-mail');
+
+// Variables para campos:
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje');
+
+// Event Listenters:
+
+eventListeners();
+function eventListeners() {
+    // Cuando la app arranca:
+    document.addEventListener('DOMContentLoaded', iniciarApp);
+
+    // Campos del formulario:
+
+    //Blur valida el formulario cuando el usuario pasa del primer input al contenedor del Formulario:
+    email.addEventListener('blur', validarFormulario);
+    asunto.addEventListener('blur', validarFormulario);
+    mensaje.addEventListener('blur', validarFormulario);
+
+    // Reinicia el formulario:
+    btnReset.addEventListener('click', resetearFormulario);
+
+    // Enviar Formulario:
+    formulario.addEventListener('submit', enviarEmail);
+}
+
+
+// Funciones:
+function iniciarApp() {
+    // console.log('Iniciando...');
+    btnEnviar.disabled = true;
+    btnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
+}
+
+// Valida el Formulario:
+
+function validarFormulario(e) {
+
+
+    if (e.target.value.length > 0) {
+
+        // Elimina los errores:
+        const error = document.querySelector('p.error');
+        if (error) {
+            error.remove();
+        }
+
+
+        e.target.classList.remove('border', 'border-red-500');
+        e.target.classList.add('border', 'border-green-500');
+    } else {
+        e.target.classList.remove('border', 'border-green-500');
+        e.target.classList.add('border', 'border-red-500');
+        mostrarError('Todos los campos son obligatorios');
+    }
+
+    if (e.target.type === 'email') {
+
+        if (er.test(e.target.value)) {
+            const error = document.querySelector('p.error');
+            if (error) {
+                error.remove();
+            }
+
+            e.target.classList.remove('border', 'border-red-500');
+            e.target.classList.add('border', 'border-green-500');
+        } else {
+            e.target.classList.remove('border', 'border-green-500');
+            e.target.classList.add('border', 'border-red-500');
+
+            mostrarError('Email no válido');
+        }
+    }
+
+    if (er.test( email.value) && asunto.value !== '' && mensaje.value !== '') {
+        btnEnviar.disabled = false;
+        btnEnviar.classList.remove('cursor-not-allowed', 'opacity-50');
+    }
+}
+
+function mostrarError(mensaje) {
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = mensaje;
+    mensajeError.classList.add('border', 'border-red-500', 'background-red-100', 'text-red-500', 'p-3', 'mt-5', 'text-center', 'error');
+
+    const errores = document.querySelectorAll('.error');
+    if (errores.length === 0) {
+        formulario.appendChild(mensajeError);
+    }
+
+
+}
+
+// Enviar el Email:
+function enviarEmail(e) {
+    e.preventDefault();
+
+
+    // Mostrar Spinner:
+    const spinner = document.querySelector('#spinner');
+    spinner.style.display = 'flex';
+
+
+    // Después de 3 segundos ocultar el spinner y mostrar el mensaje:
+    setTimeout( () => {
+        spinner.style.display = 'none';
+
+        // Mensaje de enviado con éxito:
+        const parrafo = document.createElement('p');
+        parrafo.textContent = 'El mensaje se envió correctamente';
+
+
+        // Inserta el párrafo antes del spinner
+        formulario.insertBefore(parrafo, spinner);
+        parrafo.classList.add('text-center', 'my-10', 'p-2', 'bg-green-500', 'text-white', 'font-bold', 'uppercase');
+
+        setTimeout( () => {
+            parrafo.remove(); //Elimina el mensaje de éxito después de 5 segundos.
+            resetearFormulario();
+        }, 5000);
+
+    }, 3000 );
+
+}
+
+// Función que resetea el Formulario:
+function resetearFormulario() {
+    formulario.reset();
+
+    iniciarApp();
+    eliminarBordes();
+}
+
+
+// Elimina el color verde de los inputs al resetear el Formulario:
+function eliminarBordes() {
+
+    email.classList.remove('border-green-500');
+    asunto.classList.remove('border-green-500');
+    mensaje.classList.remove('border-green-500');
+
+}
